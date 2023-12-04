58876979899903c7a08ed6b217aed947b9ebeb0c
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2023-12-04 10:26:54 +0900

    Update 매출액과 업종.py

--- 오픈소스기말프로젝트/매출액과 업종.py
+++ 오픈소스기말프로젝트/매출액과 업종.py
@@ -1,46 +1,44 @@
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
+import matplotlib
 
-matplotlib.rcParams['font.family']='Malgun Gothic'
+# Setting the font to Malgun Gothic
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'
 
+# Load the Excel file
 file_path = '수익성·생산성_분석_22년도_기준.xlsx'  # Replace with your file path
 data = pd.read_excel(file_path)
 
-# Filter out the rows with '소계' in 특성별(4) and convert columns to numeric
+# Filter and process the data
 filtered_data = data[(data['특성별(4)'] != '소계') & ~data['2022.3'].isna()]
 filtered_data = filtered_data.iloc[1:]  # Skip the first row with column names
 filtered_data['좌석 당 매출액'] = pd.to_numeric(filtered_data['2022.3'])
 
-# Grouping the data by '특성별(4)' and performing statistical analysis
+# Group and calculate statistics
 grouped_data = filtered_data.groupby('특성별(4)')['좌석 당 매출액']
 mean_data = grouped_data.mean()
-min_data = grouped_data.min()
-max_data = grouped_data.max()
-std_dev_data = grouped_data.std()
 
-# Plotting the statistical analysis results
-plt.figure(figsize=(14, 8))
+# Identify categories with the highest, lowest, and average values
+max_category = mean_data.idxmax()
+min_category = mean_data.idxmin()
+average_value = mean_data.mean()
+closest_to_average_category = (mean_data - average_value).abs().idxmin()
 
-# Mean values
-plt.subplot(1, 3, 1)
-mean_data.plot(kind='bar', color='skyblue')
-plt.title('평균 금액 per ')
+# Assign colors based on the criteria
+colors = ['grey' if category not in [max_category, min_category, closest_to_average_category] else 
+          'red' if category == max_category else 
+          'green' if category == min_category else 
+          'blue' for category in mean_data.index]
+
+# Plotting
+plt.figure(figsize=(10, 6))
+mean_data.plot(kind='bar', color=colors)
+plt.title('좌석당 매출액')
 plt.ylabel('금액 (만원)')
 
-# Minimum values
-plt.subplot(1, 3, 2)
-min_data.plot(kind='bar', color='lightgreen')
-plt.title('최소 금액 ')
-plt.ylabel('금액 (만원)')
-
-# Maximum values
-plt.subplot(1, 3, 3)
-max_data.plot(kind='bar', color='salmon')
-plt.title('최대 금액')
-plt.ylabel('금액 (만원)')
-
+# Adding a line for the average
+plt.axhline(y=average_value, color='blue', linestyle='--', label='평균에 가까운 금액')
+plt.legend()
 
 plt.tight_layout()
 plt.show()
